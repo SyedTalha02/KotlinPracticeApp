36f29c2793eae439c426ddea360ee956000f69e9
SyedTalha02 <s.talha.ssuet@gmail.com>
2019-04-02 16:57:40 +0500

    Minor Changes

--- app/src/main/java/com/kotlinsample/app/MainActivity.kt
+++ app/src/main/java/com/kotlinsample/app/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Sample Action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
